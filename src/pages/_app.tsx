4006f47fbfe6c700140bdd91f293b5175aa051a1
Eduardo Coelho <eduardocoelho619@gmail.com>
2022-10-26 21:57:57 -0300

    Ignite Shop

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
+import { Container, Header } from "../styles/pages/app"
+
+import logoImg from '../assets/logo-ignite.svg'
+
+import Image from 'next/image'
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {  
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
